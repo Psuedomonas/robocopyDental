d908a0f444a943debb4bd6d1d33ae15ee8def62e
Pseudomonas <zehm0007@d.umn.edu>
2018-03-12 17:36:57 -0500

    Update CopyData.bat

--- CopyData.bat
+++ CopyData.bat
@@ -1,4 +1,4 @@
 @REM robocopy <Source> <Destination> [<File>[ ...]] [<Options>]
-@REM probably set N to 4 depending on core number core*1 or core*2 for processes
+@REM probably set N to 4 depending on core number; N=core*1 or core*2
 robocopy <c:/Dentrix> <x:/backup> /s /MT[:N]
 robocopy <c:/Dexis> <x:/backup> /s /MT[:N]
