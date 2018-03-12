f9ab36c493fee2d392dfbbc20dbac4480d8e84be
Pseudomonas <zehm0007@d.umn.edu>
2018-03-12 17:36:13 -0500

    Add notes
    I believe @REM works for comments, but I could be wrong

--- CopyData.bat
+++ CopyData.bat
@@ -1,4 +1,4 @@
-robocopy <Source> <Destination> [<File>[ ...]] [<Options>]
-
+@REM robocopy <Source> <Destination> [<File>[ ...]] [<Options>]
+@REM probably set N to 4 depending on core number core*1 or core*2 for processes
 robocopy <c:/Dentrix> <x:/backup> /s /MT[:N]
 robocopy <c:/Dexis> <x:/backup> /s /MT[:N]
